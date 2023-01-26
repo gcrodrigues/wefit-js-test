0dc7b00893c450ec6c998f198b205d307c93566c
Gustavo Carvalho <gcrodrigues413@gmail.com>
2023-01-26 19:31:25 -0300

    feat: change cards order and set second btn green

--- js/main.js
+++ js/main.js
@@ -14,3 +14,15 @@ header.style.color = cssStyles.getPropertyValue("--white");
 
 const headerButton = header.querySelector(".btn");
 headerButton.classList.replace("btn-primary", "btn-success");
+
+/* CARDS */
+
+const cards = document.querySelectorAll(".row .col-lg-3");
+const animalCardButton = cards[0].querySelector(".btn");
+const itemsOrder = [2, 4, 3, 1];
+
+animalCardButton.classList.replace("btn-primary", "btn-success");
+
+cards.forEach((card, position) => {
+  card.style.order = itemsOrder[position];
+});
