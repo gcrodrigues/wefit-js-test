0945f059f04f46ff9a70a0fcfb49c0ab45534df9
Gustavo Carvalho <gcrodrigues413@gmail.com>
2023-01-26 19:30:43 -0300

    feat: change header colors and align content right

--- js/main.js
+++ js/main.js
@@ -1,4 +1,16 @@
-/* MENU */
+const cssStyles = getComputedStyle(document.documentElement);
+
+/* MENU NAVBAR */
 
 const navbar = document.querySelector(".btn-group-vertical");
 navbar.classList.replace("btn-group-vertical", "btn-group-horizontal");
+
+/* HEADER */
+
+const header = document.querySelector(".jumbotron");
+header.classList.add("text-right");
+header.style.backgroundColor = cssStyles.getPropertyValue("--gray");
+header.style.color = cssStyles.getPropertyValue("--white");
+
+const headerButton = header.querySelector(".btn");
+headerButton.classList.replace("btn-primary", "btn-success");
